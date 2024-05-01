0852aeef6cd5874f8f390c8a07dcd6740df1ebf6
IceBerG-15 <warmachiness13@gmail.com>
2024-05-01 11:56:16 +0530

    add procfile and modules in requirements.txt

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import RegisterForm, LoginForm, CommentForm
 from forms import CreatePostForm
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jay and mehul **** hai !!!'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -22,7 +23,7 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 # login manager
